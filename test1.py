0594af4b4a20c2dd77b1b5ff34a460ac44236eba
santi-nue <68065589+santi-nue@users.noreply.github.com>
2023-06-12 20:53:06 +0200

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,7 +1,7 @@
 import gradio as gr
 
 def hello(name):
-    return "Hello, " + name + "!"
+    return "Hello1, " + name + "!"
 
 iface = gr.Interface(fn=hello, inputs="text", outputs="text")
 iface.launch()
